9d3e1ebda23041cd056bfa6c77c571f5223bf179
cmtzvitela <cmtzvitela@gmail.com>
2021-12-09 22:07:02 -0600

    Extra funcionalidad al Navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,10 +5,23 @@ import "./NavBar.css";
 
 export default function NavBar() {
     const [active, setActive] = useState(0)
-
+    let content = ""
     const handleClick = (element) => {setActive(element)}
 
+    switch(active) {
+        case 1: content = `Random content ${active}`
+        break;
+        case 2: content = `Random content ${active}`
+        break;
+        case 3: content = `Random content ${active}`
+        break;
+        case 4: content = `Random content ${active}`
+        break;
+        default: content = `No content`
+    }
+
     const checkActive = (element) => active === element ? 'li-active': null;
+    const checkActiveContent = (element) => (active === element ? true:false);
     return (
         <div>
             <ul>
@@ -16,6 +29,7 @@ export default function NavBar() {
                 <li className={checkActive(2)} onClick= {() => handleClick(2)}>Item 2</li>
                 <li className={checkActive(3)} onClick= {() => handleClick(3)}>Item 3</li>
                 <li className={checkActive(4)} onClick= {() => handleClick(4)}>Item 4</li>
+                <div>{checkActiveContent(active) ? <h1>{content}</h1> : null}</div>
             </ul>
         </div>
     )
